4afd4d0668c018a0454d0a67c765ee4a4395eabb
abdulmazidakash <akashabdulmazid01@gmail.com>
2024-12-26 20:43:16 +0600

    final commit

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const verifyToken = (req, res, next) =>{
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
 	const db = client.db('artifact-db');
 	const artifactCollection = db.collection('artifacts');
